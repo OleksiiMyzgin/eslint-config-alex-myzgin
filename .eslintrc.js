48c1d449392c8fae311353adc678145ffd30fa36
OleksiiMyzgin <Oleksii.Myzgin@care.com>
2021-09-25 20:14:37 +0200

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,123 @@
+module.exports = {
+  extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
+  plugins: ['html', 'prettier', 'react-hooks'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    jquery: true,
+    jest: true,
+    es6: true,
+  },
+  rules: {
+    // 'no-await-in-loop': 0,
+    'no-debugger': 2,
+    'no-alert': 1,
+    'no-return-assign': ['error', 'except-parens'],
+    'no-restricted-syntax': [
+      2,
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-unused-vars': [
+      1,
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: 'res|next|^err',
+      },
+    ],
+    'no-unused-expressions': [
+      2,
+      {
+        allowTaggedTemplates: true,
+      },
+    ],
+    'no-param-reassign': [
+      2,
+      {
+        props: false,
+      },
+    ],
+    'no-console': 1,
+    'no-shadow': [
+      2,
+      {
+        hoist: 'all',
+        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
+      },
+    ],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+      },
+    ],
+    'arrow-body-style': [2, 'as-needed'],
+    'import/prefer-default-export': 0,
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    // import: 0,
+    // 'func-names': 0,
+    // 'space-before-function-paren': 0,
+    // 'comma-dangle': 0,
+    // 'max-len': 0,
+    // 'import/extensions': 0,
+    // 'no-underscore-dangle': 0,
+    // 'consistent-return': 0,
+    // 'react/no-array-index-key': 0,
+    // 'react/no-unescaped-entities': 0,
+    'react/display-name': 1,
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/forbid-prop-types': 0,
+    'jsx-a11y/accessible-emoji': 0,
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either',
+      },
+    ],
+    'react/require-default-props': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.mdx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 80,
+        // below line only for windows users facing CLRF and eslint/prettier error
+        // non windows users feel free to delete it
+        endOfLine: 'auto',
+      },
+    ],
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        aspects: ['invalidHref'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
+    '@typescript-eslint/comma-dangle': ['off'],
+  },
+};
---
+++ typescript.js
@@ -0,0 +1,37 @@
+module.exports = {
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses rules from `@typescript-eslint/eslint-plugin`,
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // Layer in all the JS Rules
+    './.eslintrc.js',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // This allows us to use async function on addEventListener().
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 1,
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': [
+      'warn',
+      {
+        ignoreDeclarationMerge: true,
+      },
+    ],
+    '@typescript-eslint/no-floating-promises': 'off',
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+};
